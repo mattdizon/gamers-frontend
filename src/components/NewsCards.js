65f642c223e5160ed1b261f1fda62f6141b36f2b
github username <matthewdizon94@protonmail.com>
2019-03-13 13:59:00 -0400

    added news api

--- src/components/NewsCards.js
+++ src/components/NewsCards.js
@@ -1,16 +1,52 @@
 import React from 'react'
 
 class NewsCards extends React.Component{
-    render(){
-        return (
-            <a href = {this.props.article.url}>
+    imgRender = () =>{
+        if(this.props.article.thumbnail === undefined){
+            return <img className="card-img-top" src = "https://www.grandjunctionmartialarts.com/wp-content/uploads/2017/04/default-image.jpg" alt="Card image cap"/>
+        }
+        else{
+            return <img className="card-img-top" src = {this.props.article.thumbnail.url} alt="Card image cap"/>
+        }
+    }
+    filteredRender = () =>{
+        switch(this.props.game){
+            case "League of Legends":
+                return<a href = {this.props.article.url}>
+                  <div className="card" style={{width: "18rem", border:"none", marginRight:'5vw'}}>
+                    <img className="card-img-top" src = {this.props.article.urlToImage} alt="Card image cap"/>
+                    <div className="card-body">
+                        <p className="card-text" style = {{color:'#000000',fontSize:'1.5vh' }}>{this.props.article.title}</p>
+                    </div>
+                </div>
+                </a>
+                break;
+            case"Overwatch":
+
+                return <a href = {this.props.article.defaultUrl}>
+                        <div className="card" style={{width: "18rem", border:"none", marginRight:'5vw'}}>
+                            {this.imgRender()}
+                            <div className="card-body">
+                                <p className="card-text" style = {{color:'#000000',fontSize:'1.5vh' }}>{this.props.article.title}</p>
+                            </div>
+                        </div>
+                    </a>
+
+                    break;
+            default:
+            return <a href = {this.props.article.url}>
               <div className="card" style={{width: "18rem", border:"none", marginRight:'5vw'}}>
                 <img className="card-img-top" src = {this.props.article.urlToImage} alt="Card image cap"/>
                 <div className="card-body">
                     <p className="card-text" style = {{color:'#000000',fontSize:'1.5vh' }}>{this.props.article.title}</p>
                 </div>
             </div>
-        </a>)
+            </a>
+            }
+    }
+    render(){
+        console.log(this.props)
+        return (this.filteredRender())
     }
 }
 export default NewsCards
--- src/containers/News.js
+++ src/containers/News.js
@@ -14,18 +14,66 @@ class News extends React.Component{
         .then(news => this.setState({news:news.articles}))
     }
     renderCards = () =>{
-        return this.state.news.map(article => <NewsCard article = {article}/>)
+
+        return this.state.news.map(article => <NewsCard article = {article} game = {this.state.selectedGame}/>)
     }
     renderEsports = () =>{
         return this.state.games.map(game => <option>{game}</option>)
     }
     changeGame = (e) =>{
-        this.setState({selectedGame: e.target.value})
+        this.setState({selectedGame: e.target.value},() =>{
+            this.renderFilteredGame()
+        })
 
+    }
+    renderFilteredGame = () =>{
+        switch(this.state.selectedGame){
+            case 'League of Legends':
+            fetch('https://newsapi.org/v2/everything?q="+league%20of%20legends"&apiKey=b252f4fed7984135a2823c31a50f3c74')
+            .then(resp => resp.json())
+            .then(news => this.setState({news:news.articles}))
+                break
+            case 'Overwatch':
+            fetch('https://api.overwatchleague.com/news?page=69')
+            .then(resp => resp.json())
+            .then(news => this.setState({news:news.blogs}))
+                break
+            case 'Dota 2':
+            case 'League of Legends':
+            fetch('https://newsapi.org/v2/everything?q="Dota+2"&apiKey=b252f4fed7984135a2823c31a50f3c74')
+            .then(resp => resp.json())
+            .then(news => this.setState({news:news.articles}))
+                break
+            case 'CSGo':
+            fetch('https://newsapi.org/v2/everything?q="CS:Go"&apiKey=b252f4fed7984135a2823c31a50f3c74')
+            .then(resp => resp.json())
+            .then(news => this.setState({news:news.articles}))
+                break
+            case 'Hearthstone':
+            fetch('https://newsapi.org/v2/everything?q="Hearthstone"&apiKey=b252f4fed7984135a2823c31a50f3c74')
+            .then(resp => resp.json())
+            .then(news => this.setState({news:news.articles}))
+                break
+            case 'Starcraft 2':
+            fetch('https://newsapi.org/v2/everything?q="Starcraft+2"&apiKey=b252f4fed7984135a2823c31a50f3c74')
+            .then(resp => resp.json())
+            .then(news => this.setState({news:news.articles}))
+                break
+            case 'Super Smash Bros':
+            fetch('https://newsapi.org/v2/everything?q="Super+Smash+Bros"&apiKey=b252f4fed7984135a2823c31a50f3c74')
+            .then(resp => resp.json())
+            .then(news => this.setState({news:news.articles}))
+                break
+            default:
+            fetch('https://newsapi.org/v2/top-headlines?sources=polygon&apiKey=b252f4fed7984135a2823c31a50f3c74')
+            .then(resp => resp.json())
+            .then(news => this.setState({news:news.articles}))
+
+        }
     }
 
     render(){
-        console.log(this.state.news)
+
         return(
             <div>
             <h1>Gamer News</h1>
